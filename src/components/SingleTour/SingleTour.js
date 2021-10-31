3104c3446310d6c4dd96694fc74451537870016d
Simanta Barua <simanta.barua@yhaoo.com>
2021-10-31 06:38:35 +0600

    tour

---
+++ src/components/SingleTour/SingleTour.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Badge, Button, Card, Col, ListGroup } from 'react-bootstrap';
+import './SingleTour.css'
+import { Link } from 'react-router-dom';
+
+const SingleTour = (props) => {
+    const { index, title, image, category, price, details } = props.product;
+    return (
+        <div>
+             <Col>
+                <Card style={{ width: '18rem' }} className="custom-cart">
+                    <Card.Img variant="top" src={image} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>
+                        </Card.Text>
+                        <ListGroup variant="flush" >
+                            <ListGroup.Item> Category: <Badge pill bg="primary">{category}</Badge></ListGroup.Item>
+                            <ListGroup.Item>Price: ${price} </ListGroup.Item>
+                            <ListGroup.Item>{details} </ListGroup.Item>
+                        </ListGroup>
+                    </Card.Body>
+                    <Card.Footer className="text-center card-footer">
+                        <Link to={`/tour_details/${index}`} className="text-light"> <Button variant="primary">Details</Button></Link>
+                    </Card.Footer>
+                </Card>
+            </Col>
+        </div>
+    );
+};
+
+export default SingleTour;
--- src/components/Tours/Tours.js
+++ src/components/Tours/Tours.js
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { Container, Row } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import SingleTour from '../SingleTour/SingleTour';
 const Tours = () => {
+    const [products, setProducts] = useState([])
+    const url = 'https://intense-ravine-02304.herokuapp.com/tours'
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setProducts(data)
+            )
+    }, [])
     return (
-        <div>
-            <h1>tours</h1>
-        </div>
+        <Container>
+            <div className="course-container mt-5 p-3">
+                <div className="text-center text-box ">
+                    <h2> Shop whatever you want the Right </h2>
+                    <h1>Our latest medical items for you</h1>
+                </div>
+                <Row xs={1} md={2} sm={1} lg={4} className="g-1 mt-2">
+                    {
+                        products.map(product => <SingleTour
+                            product={product}
+                        ></SingleTour>)
+                    }
+                </Row>
+            </div>
+        </Container>
     );
 };
 
